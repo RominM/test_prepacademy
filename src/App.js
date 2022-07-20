2f2178cc874f632bf51d5f0072f50aa2b51eaf84
RominM <rominmanogil.off@gmail.com>
2022-07-20 17:05:12 +0200

    feat: :tada: initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './assets/svg/logo.svg';
+import './App.css';
+import Jsx1 from 'view/jsx1/Jsx1';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Jsx1 />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
